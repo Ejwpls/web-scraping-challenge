5620ef79763bfcf10f7ab691e8b0697922536e92
e'ddddddddddd <98539748+Ejwpls@users.noreply.github.com>
2022-06-26 13:49:53 +0800

    Final

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -11,7 +11,10 @@ import pandas as pd
 
 def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    # Intialise empty dictionary 
+    mars_data= {}
 
     # Visit website
     url = 'https://redplanetscience.com/'
@@ -33,12 +36,6 @@ def scrape():
         news_title = result.find_all('div',class_='content_title')[0].text
         news_p = result.find_all('div',class_='article_teaser_body')[0].text
 
-    # Dictionary 
-    post = {
-        'title': news_title,
-        'parap': news_p,
-    }
-
     # Visit https://spaceimages-mars.com/
     url = 'https://spaceimages-mars.com/'
     browser.visit(url)
@@ -52,12 +49,13 @@ def scrape():
     # Retrieve the parent divs for all articles
     img_result = soup.find_all('img', class_="headerimage fade-in")
 
-    featured_image_url = img_result[0]['src']
+    # Import 2 - link - Featured Mars Image
+    featured_image_url = url + img_result[0]['src']
 
     # Use pandas to scrape website
     url = 'https://galaxyfacts-mars.com/'
     tables = pd.read_html(url)
-    # Convert the data to a HTML table string
+    # Convert the data to a HTML table string - Import 3  - Mars Facts
     html_table = tables[1].to_html()
 
     url = 'https://marshemispheres.com/index.html'
@@ -83,6 +81,7 @@ def scrape():
 
     i = 0
 
+    #Import 4  - Mars Hemisphere
     hemisphere_image_urls = []
 
     for title_url in book_url_list:
@@ -93,7 +92,7 @@ def scrape():
         results = soup.find_all('div', class_="downloads")
         img_link =  'https://marshemispheres.com/' + results[0].find('a')['href']
 
-        print(title_lst[i], img_link)
+        #print(title_lst[i], img_link)
 
         # Data to export
         mars_hemisphere_data = {
@@ -105,13 +104,16 @@ def scrape():
 
         i += 1
 
+    mars_data = {
+        'title': news_title,
+        'parap': news_p,
+        'featured_mars_image' : featured_image_url,
+        'mars_facts': html_table,
+        'mars_hemi': hemisphere_image_urls
+    }
+
     browser.quit()
 
-    return
+    return mars_data
 
-
-# Initialize PyMongo to work with MongoDBs
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-
-scrape()
+mars_data = scrape()
